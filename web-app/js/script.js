6cfcbf5a9e836e7e85e36e8182c03bb014fdb297
theDanielK <dankruss@gmail.com>
2013-06-29 00:53:12 +0200

    home page

--- web-app/js/script.js
+++ web-app/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-//Nav
+    //Nav
     $('.nav_sheen').on('mouseover', function(){
         $this = $(this);
         position = parseInt($this.position()['left']) + ($this.width() / 3*2);
-        $('#nav_sheen').stop().animate({left: position}, 300, 'swing');
+        $('#nav_sheen').stop().animate({left: position}, 300);
         $('#nav_sheen').fadeIn();
     });
 
@@ -11,6 +11,13 @@ $(document).ready(function(){
         $('#nav_sheen').animate({left: 21}, 300, 'swing');
     });
 
+    //Footer
+    $('.footer_social ul a img').on('mouseover', function(){
+        $(this).animate({width: '30', height: '30'}, 200);
+    });
+    $('.footer_social ul a img').on('mouseout', function(){
+        $(this).animate({width: '27', height: '27'}, 200);
+    });
 
 
 });
