9720ba6f7b1ef3ebf7a31bfc8089e11b9ecf7b0b
theDanielK <dankruss@gmail.com>
2013-06-29 00:40:02 +0200

    home page

--- web-app/js/script.js
+++ web-app/js/script.js
@@ -3,9 +3,8 @@ $(document).ready(function(){
     $('.nav_sheen').on('mouseover', function(){
         $this = $(this);
         position = parseInt($this.position()['left']) + ($this.width() / 3*2);
-        $('#nav_sheen').animate({left: position}, 300, 'swing');
+        $('#nav_sheen').stop().animate({left: position}, 300, 'swing');
         $('#nav_sheen').fadeIn();
-        console.log($this.width());
     });
 
     $('.container div').not('.nav_sheen').on('mouseover', function(){
